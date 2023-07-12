f71c963b56805ce91a528ad7f11c84fa1358adb4
Alessandro <alessandrosponte@gmail.com>
2023-07-12 16:32:21 -0300

    Refatora o segundo code smells - linhas em branco

--- backend/ride/src/main.ts
+++ backend/ride/src/main.ts
@@ -1,39 +1,29 @@
 // @ts-nocheck
 import express from "express";
 const app = express();
+
 app.use(express.json());
 
 // calculate ride price
 app.post("/calculate_ride", function(req, res){
     let price = 0;
-
-    for (const segment of req.body.segments) {
-        
+    for (const segment of req.body.segments) {        
         segment.date = new Date(segment.date);
-
         if (segment.distance != null && segment.distance != undefined && typeof segment.distance === "number" && segment.distance > 0) {
-
             if (segment.date != null && segment.date != undefined && segment.date instanceof Date && segment.date.toString() !== "Invalid Date") {
-
                 // overnight
-
                 if (segment.date.getHours() >= 22 || segment.date.getHours() <= 6) {
-
                     // not sunday
                     if (segment.date.getDay() !== 0) {
-
                         price += segment.distance * 3.90;
                         // sunday
                     } else {
                         price = segment.distance * 5;
-
                     }                   
                 } else {
                     // sunday
                     if(segment.date.getDay() === 0) {
-
                         price += segment.distance * 2.9;
-
                     } else {
                         price += segment.distance * 2.10;
                     }
@@ -45,7 +35,6 @@ app.post("/calculate_ride", function(req, res){
             }
         } else {
             // console.log(req.body.dist);
-
             res.json({ price: -1 });
             return;
         }
