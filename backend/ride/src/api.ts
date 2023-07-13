b6a91bb67d0e0d3e679d88c659f086dc53001140
Alessandro <alessandrosponte@gmail.com>
2023-07-13 18:15:42 -0300

    Entrega do desafio da aula 1 - feito pelo professor

--- backend/ride/src/api.ts
+++ backend/ride/src/api.ts
@@ -1,11 +1,13 @@
-// @ts-nocheck
 import express from "express";
+import pgp from 'pg-promise';
 import Ride from "./Ride"
+import Passenger from "./Passenger";
+import crypto from 'crypto';
 const app = express();
 
 app.use(express.json());
 
-app.post("/calculate_ride", function(req, res){    
+app.post("/calculate_ride", function(req , res){    
     try{        
         const ride = new Ride();
         for (const segment of req.body.segments) {
@@ -13,10 +15,69 @@ app.post("/calculate_ride", function(req, res){
         }
         const price = ride.calculate();
         res.json({ price });
-    } catch (e) {
+    } catch (e: any) {
         res.status(422).send(e.message);
     }
-    return;
+});
+
+app.post("/passengers", async function(req, res){    
+    try{
+        const input = req.body;
+        const passengerId = crypto.randomUUID();
+        const connection = pgp()("postgres://postgres@localhost:5432/app");
+        await connection.query('insert into cccat12.passenger (passenger_id, name, email, document) values ($1, $2, $3, $4)', [passengerId, input.name, input.email, input.document]);
+        await connection.$pool.end();
+        // const passenger = new Passenger(input.name, input.email, input.document);                
+        res.json({ passengerId });
+    } catch (e: any) {
+        res.status(422).send(e.message);
+    }
+});
+
+app.get("/passengers/:passengerId", async function(req, res){
+    try{
+        const input = req.params;        
+        const connection = pgp()("postgres://postgres@localhost:5432/app");
+        const [passengerData] = await connection.query('select * from cccat12.passenger where passenger_id = $1', [input.passengerId]);
+        await connection.$pool.end();
+        // const passenger = new Passenger(input.name, input.email, input.document);                
+        res.json( passengerData );
+    } catch (e: any) {
+        res.status(422).send(e.message);
+    }    
+});
+
+app.post("/drivers", async function(req, res){    
+    try{
+        const input = req.body;
+        const driverId = crypto.randomUUID();
+        const connection = pgp()("postgres://postgres@localhost:5432/app");
+        await connection.query('insert into cccat12.driver (driver_id, name, email, document, car_plate) values ($1, $2, $3, $4, $5)', [driverId, input.name, input.email, input.document, input.carPlate]);
+        await connection.$pool.end();
+        // const passenger = new Passenger(input.name, input.email, input.document);                
+        res.json({ driverId });
+    } catch (e: any) {
+        res.status(422).send(e.message);
+    }
+});
+
+app.get("/drivers/:driverId", async function(req, res){
+    try{
+        const input = req.params;        
+        const connection = pgp()("postgres://postgres@localhost:5432/app");
+        const [driverData] = await connection.query('select * from cccat12.driver where driver_id = $1', [input.driverId]);
+        await connection.$pool.end();
+        // const passenger = new Passenger(input.name, input.email, input.document);                
+        res.json({ 
+            driverId: driverData.driver_id,
+            name: driverData.name,
+            email: driverData.email,
+            document: driverData.document,
+            carPlate: driverData.car_plate
+        });
+    } catch (e: any) {
+        res.status(422).send(e.message);
+    }    
 });
 
 app.listen(3000);
--- backend/ride/test/api.test.ts
+++ backend/ride/test/api.test.ts
@@ -10,8 +10,8 @@ test("Deve fazer o cálculo do preço de uma corrida durante o dia", async funct
         { distance: 10, date: "2021-03-01T10:00:00"}
         ]
     };
-    const response = axios.post("http://localhost:3000/calculate_ride", input);
-    const output = (await response).data;
+    const response = await axios.post("http://localhost:3000/calculate_ride", input);
+    const output = await response.data;
     expect(output.price).toBe(21);
 });
 
@@ -21,8 +21,89 @@ test("Se a distância for inválida deve lançar um erro", async function () {
         { distance: -10, date: "2021-03-01T10:00:00"}
         ]
     };
-    const response = axios.post("http://localhost:3000/calculate_ride", input);
-    expect((await response).status).toBe(422);
-    const output = (await response).data;
+    const response = await axios.post("http://localhost:3000/calculate_ride", input);
+    expect(response.status).toBe(422);
+    const output = response.data;
     expect(output).toBe("Invalid distance");
+});
+
+test("Deve cadastrar o passageiro quando informado todos os dados válidos", async function () {    
+    const input = {
+        name: 'Alessandro Ponte',
+        email: 'alessandrosponte@gmail.com',
+        document: '17547870376'
+    };
+    const response1 = await axios.post("http://localhost:3000/passengers", input);
+    const output1 = response1.data;
+    expect(output1.passengerId).toBeDefined();    
+});
+
+test.skip("Não deve cadastrar o passageiro com o cpf inválido", async function () {    
+    const input = {
+        name: 'Alessandro Ponte',
+        email: 'alessandrosponte@gmail.com',
+        document: '17547870374'
+    };
+    const response = await axios.post("http://localhost:3000/passengers", input);
+    expect(response.status).toBe(422);
+    const output = response.data;
+    expect(output).toBe('Invalid cpf');
+});
+
+test("Deve obter o passageiro", async function () {    
+    const input = {
+        name: 'Alessandro Ponte',
+        email: 'alessandrosponte@gmail.com',
+        document: '17547870376'
+    };
+    const response1 = await axios.post("http://localhost:3000/passengers", input);
+    const output1 = response1.data;    
+    const response2 = await axios.get(`http://localhost:3000/passengers/${output1.passengerId}`);
+    const output2 = response2.data;    
+    expect(output2.name).toBe('Alessandro Ponte');
+    expect(output2.email).toBe('alessandrosponte@gmail.com');
+    expect(output2.document).toBe('17547870376');
+});
+
+test("Deve cadastrar o motorista", async function () {    
+    const input = {
+        name: 'Alessandro Ponte',
+        email: 'alessandrosponte@gmail.com',
+        document: '17547870376',
+        carPlate: 'AAA9999'
+    };
+    const response = await axios.post("http://localhost:3000/drivers", input);
+    const output = response.data;    
+    expect(output.driverId).toBeDefined();
+});
+
+
+test.skip("Não deve cadastrar o motorista com cpf inválido", async function () {    
+    const input = {
+        name: 'Alessandro Ponte',
+        email: 'alessandrosponte@gmail.com',
+        document: '17547870376',
+        carPlate: 'AAA9999'
+    };
+    const response = await axios.post("http://localhost:3000/drivers", input);    
+    expect(response.status).toBe(422);
+    const output = response.data;
+    expect(output).toBe('Invalid cpf');
+});
+
+test("Deve obter o motorista", async function () {    
+    const input = {
+        name: 'Alessandro Ponte',
+        email: 'alessandrosponte@gmail.com',
+        document: '17547870376',
+        carPlate: 'AAA9999'
+    };
+    const response1 = await axios.post("http://localhost:3000/drivers", input);
+    const output1 = response1.data;    
+    const response2 = await axios.get(`http://localhost:3000/drivers/${output1.driverId}`);
+    const output2 = response2.data;    
+    expect(output2.name).toBe('Alessandro Ponte');
+    expect(output2.email).toBe('alessandrosponte@gmail.com');
+    expect(output2.document).toBe('17547870376');
+    expect(output2.carPlate).toBe('AAA9999');
 });
